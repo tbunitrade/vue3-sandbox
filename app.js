35bb054c270d7e91d184d3d6b0a3d214737c2e8d
tbunitrade <tbunitrade@gmail.com>
2024-08-04 13:29:37 +0300

    prevent few methods

--- app.js
+++ app.js
@@ -4,16 +4,28 @@ Vue.createApp({
         myHtml: '<h3>Vue3app</h3>',
         //first  model
         title: 'I am title',
-        items: [1, 2, 3, 4, 5, 6, 7]
+        items: [1, 2]
     }),
     computed: {
         eventItems(){
             return this.items.filter(i=> i % 2 !== 0)
         }
-    }
-    // methods: {
+    },
+    methods: {
+        addItem(event) {
+            this.items.unshift(this.$refs.myInput.value)
+            this.$refs.myInput.value = ''
+            console.log(event)
+            //console.log(this.$refs.myInput.value)
+        },
+        remove(i) {
+            this.items.splice(i,1)
+        },
+        log(item) {
+            console.log('Log item', item);
+        }
     //     stopPropagation(event) {
     //         event.stopPropagation()
     //     }
-    // }
+    }
 }).mount('#app');
