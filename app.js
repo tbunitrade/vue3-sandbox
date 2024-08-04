99736a0eb950a9ba8a0fceefae84b5a3db9a5318
tbunitrade <tbunitrade@gmail.com>
2024-08-04 11:13:24 +0300

    Computed ability to filter the List items

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ Vue.createApp({
     data: () => ({
         myHtml: '<h3>Vue3app</h3>',
         //first  model
-        title:'I am title',
-        items: [1,2,3,4,5,6,7]
+        title: 'I am title',
+        items: [1, 2, 3, 4, 5, 6, 7]
     }),
+    computed: {
+        eventItems(){
+            return this.items.filter(i=> i % 2 !== 0)
+        }
+    }
     // methods: {
     //     stopPropagation(event) {
     //         event.stopPropagation()
