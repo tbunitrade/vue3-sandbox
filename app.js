8ae8c86ba08131d7f390dc32b9a848803e217e8f
tbunitrade <tbunitrade@gmail.com>
2024-08-04 14:12:38 +0300

    virtual dom

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
-Vue.createApp({
+const app = Vue.createApp({
 
-    data: () => ({
-        myHtml: '<h3>Vue3app</h3>',
-        //first  model
-        title: 'I am title',
-        items: [1, 2]
-    }),
-    computed: {
-        eventItems(){
-            return this.items.filter(i=> i % 2 !== 0)
+    data() {
+        return {
+            title: 'Template abbility'
         }
     },
-    methods: {
-        addItem(event) {
-            this.items.unshift(this.$refs.myInput.value)
-            this.$refs.myInput.value = ''
-            console.log(event)
-            //console.log(this.$refs.myInput.value)
-        },
-        remove(i) {
-            this.items.splice(i,1)
-        },
-        log(item) {
-            console.log('Log item', item);
-        }
-    //     stopPropagation(event) {
-    //         event.stopPropagation()
-    //     }
-    }
-}).mount('#app');
+    template: `
+        <div class="card center">
+            <h1>{{title}}</h1>
+            <button class="btn" @click="title='Title lost'" >Change</button>
+        </div>
+    `
+})
+
+app.mount('#app')
