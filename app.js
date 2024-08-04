37d6806488bb6baa277970e7f30feefe61af6643
tbunitrade <tbunitrade@gmail.com>
2024-08-04 16:18:33 +0300

    How react app do with PROXU , як працює рекативність

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
+const  h = Vue.h
 const app = Vue.createApp({
+
 
     data() {
         return {
             title: 'Template abbility'
         }
     },
-    template: `
-        <div class="card center">
-            <h1>{{title}}</h1>
-            <button class="btn" @click="title='Title lost'" >Change</button>
-        </div>
-    `
+
+    methods : {
+        changeTitle() {
+            console.log(this)
+            this.title = 'Changed !';
+        }
+    },
+    // template: `
+    //     <div class="card center">
+    //         <h1>{{title}}</h1>
+    //         <button class="btn" @click="title='Title lost'" >Change</button>
+    //     </div>
+    // `
+    render(){
+        return h('div', {
+            className: 'card center'
+        }, [
+            h('h1', {} , this.title),
+            h('button', {
+                class:'btn',
+                onClick: this.changeTitle
+            }, 'change')
+        ])
+    }
 })
 
 app.mount('#app')
+
+let title = 'MVP version VUE'
+let msg = 'Title of this : ' + title
+console.log('msg ', msg)
